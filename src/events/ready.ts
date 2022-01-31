bd2017a033d0ff02e979ad36a63fd55a176caa6b
Andrew Whigham <awhigham9>
2022-01-31 15:04:42 -0500

    Add IEvent interface and refactor to use it

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,7 +1,15 @@
-module.exports = {
+import { Client } from "discord.js";
+import { IEvent } from "../ievent";
+
+export default <IEvent> {
 	name: 'ready',
 	once: true,
-	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
+	execute(client : Client): void {
+		if (!client.user) {
+			console.log("ERROR: Client user not found\nReady!");
+		}
+		else {
+			console.log(`Ready! Logged in as ${client.user.tag}`);
+		}
 	},
 };
